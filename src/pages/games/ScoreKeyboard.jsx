1ba12a2465a4732febd15e7aeec09f97e801f8ce
trongnam13823 <trantrongnam138@gmail.com>
2026-03-18 17:41:12 +0700

    sdsdsd

--- src/pages/games/ScoreKeyboard.jsx
+++ src/pages/games/ScoreKeyboard.jsx
@@ -87,7 +87,7 @@ export default function ScoreKeyboard({
 
       {/* Numpad */}
       <div className='grid grid-cols-5 gap-3'>
-        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 0].map((num) => (
+        {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9].map((num) => (
           <Button
             key={num}
             onClick={() => onKeyPress(num)}
